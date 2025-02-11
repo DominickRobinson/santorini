f0b4b679b57848c5e54c912926baeb3ee10b1407
Dominick Robinson <drobinson.lbe@gmail.com>
2025-02-10 21:41:03 -0500

    Add Worker.java

---
+++ src/main/java/com/santorini/game/Worker.java
@@ -0,0 +1,27 @@
+package com.santorini.game;
+
+import com.santorini.board.Field;
+import java.util.List;
+
+public class Worker {
+    private Field field;
+
+    public Worker (Field startField) {
+        this.field = startField;
+        this.field.assignWorker(this);
+    }
+
+    public Field getField() {
+        return this.field;
+    }
+
+    public void moveTo(Field newField) {
+        this.field.removeWorker();
+        this.field = newField;
+        this.field.assignWorker(this);
+    }
+
+    public void buildAt(Field field) {
+        field.getTower().build();
+    }
+}
