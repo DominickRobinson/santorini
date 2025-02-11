6c115332fbbd300c3b667e16d08ce730cab76bc6
Dominick Robinson <drobinson.lbe@gmail.com>
2025-02-10 21:40:48 -0500

    Add Tower.java

---
+++ src/main/java/com/santorini/board/Tower.java
@@ -0,0 +1,28 @@
+package com.santorini.board;
+
+public class Tower {
+    private int levels;
+    private static final int MAX_LEVELS = 4;
+
+
+    public Tower () {
+        this.levels = 0;
+    }
+
+    public void build () {
+        if (this.levels < this.MAX_LEVELS) {
+            this.levels++;
+        } else {
+            throw new IllegalStateException("Cannot build when a tower already has a dome!");
+        }
+    }
+
+    public int getLevels() {
+        return this.levels;
+    }
+
+    public boolean hasDome() {
+        return this.levels == this.MAX_LEVELS;
+    }
+
+}
