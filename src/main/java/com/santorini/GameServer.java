ef53f1a349a7db70b0e1f83577c822bc78a87048
DominickRobinson <72145105+DominickRobinson@users.noreply.github.com>
2025-04-09 23:08:54 -0400

    implemented GameServer class

--- src/main/java/com/santorini/GameServer.java
+++ src/main/java/com/santorini/GameServer.java
@@ -1,19 +1,26 @@
 package com.santorini;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
 import org.json.JSONObject;
 
+import com.santorini.board.Position;
+import com.santorini.board.Tile;
 import com.santorini.game.Game;
+import com.santorini.game.Game.Phase;
+
 import fi.iki.elonen.NanoHTTPD;
 
 public class GameServer extends NanoHTTPD {
     
-    private final Game game = new Game();
+    private Game game = new Game();
 
     public GameServer(int port) throws IOException {
         super(port);
         start(SOCKET_READ_TIMEOUT, false);
-        System.out.println("Game running on http://localhost:/" + port + "\n");
+        System.out.println("Game running on http://localhost:" + port + "\n");
     }
 
     @Override
@@ -21,44 +28,44 @@ public class GameServer extends NanoHTTPD {
         String uri = session.getUri();
         Method method = session.getMethod();
 
+        System.out.println("Received request: " + session.getUri());
+
         try {
 
-            switch (uri) {
-            case "/game-state":
-                if (Method.GET.equals(method)) {
-                    JSONObject json = game.toJSON();
-                    return finishJSONResponse(json, Response.Status.OK);
-                }
+            if (Method.OPTIONS.equals(method)) {
+                Response res = newFixedLengthResponse(Response.Status.OK, "application/json", "");
+                res.addHeader("Access-Control-Allow-Origin", "*");
+                res.addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+                res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+                return res;
+            }
 
-                break;
+            if ("/game-state".equals(uri) && Method.GET.equals(method)) {
+                return finishJSONResponse(game.toJSON(), Response.Status.OK);
+            }
+                         
+            if ("/tile-press".equals(uri) && Method.POST.equals(method)) {
 
-            case "/spawn":
-                if (Method.POST.equals(method)) {
-                    // TODO: add stuff here
-                    break;
-                }
+                System.out.println(" should press tile...");
 
-                break;
+                Map<String, String> body = new HashMap<>();
+                session.parseBody(body);
+                String requestBody = body.get("postData");
 
-            case "/move":
-                if (Method.POST.equals(method)) {
-                    // TODO: add stuff here
-                    break;
-                }
+                JSONObject requestJson = new JSONObject(requestBody);
+                int x = requestJson.getInt("x");
+                int y = requestJson.getInt("y");
 
-                break;
+                System.out.println(" should handle tile press...");
 
-            case "/build":
-                if (Method.POST.equals(method)) {
-                    // TODO: add stuff here
-                    break;
-                }
+                JSONObject result = handleTilePress(x, y);
 
-                break;
+                return finishJSONResponse(result, Response.Status.OK);
+            }
 
-            default:
-                break;
-
+            if ("/new-game".equals(uri) && Method.POST.equals(method)) {
+                resetGame();
+                return finishJSONResponse(game.toJSON(), Response.Status.OK);
             }
 
             JSONObject error = new JSONObject();
@@ -75,14 +82,64 @@ public class GameServer extends NanoHTTPD {
 
         }
     
-
-
     }
 
     private Response finishJSONResponse(JSONObject json, Response.Status status) {
         Response res = newFixedLengthResponse(status, "application/json", json.toString());
         res.addHeader("Access-Control-Allow-Origin", "*");
         return res;
+    }
+
+    private JSONObject handleTilePress(int x, int y) {
+
+        Tile tile = game.getTileAt(new Position(x, y));
+        Game.Phase phase = game.getPhase();
+
+        System.out.println("handleTilePress()");
+
+        if (phase == Phase.SPAWN) {
+            System.out.print(" SPAWN...");
+            boolean spawned = game.trySpawn(tile);
+            if (spawned)
+                System.out.println("  successful!");
+            else
+                System.out.println("  failed!");
+        } else if (phase == Phase.SELECT) {
+            System.out.print(" SELECT...");
+            boolean selected = game.trySelect(tile);
+            if (selected)
+                System.out.println("  successful!");
+            else
+                System.out.println("  failed!");
+        } else if (phase == Phase.MOVE) {
+            System.out.print(" MOVE...");
+            if (game.getSelectedWorker() != null) {
+                System.out.print(" (player selected) ");
+                boolean moved = game.tryMove(tile);
+                if (moved)
+                    System.out.println("  successful!");
+                else
+                    System.out.println("  failed!");
+            } else {
+                System.out.print(" (no player selected) ");
+                if (game.getCurrentPlayer().ownsWorker(tile.getWorker())) {
+                    game.setSelectedWorker(tile.getWorker());
+                }
+            }
+        } else if (phase == Phase.BUILD) {
+            System.out.print(" BUILD...");
+            boolean built = game.tryBuild(tile);
+            if (built)
+                System.out.println("  successful!");
+            else
+                System.out.println("  failed!");
+        }
+
+        return game.toJSON();
+    }
+
+    private void resetGame() {
+        this.game = new Game();
     }
 
 }
